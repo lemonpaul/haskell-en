cfe398d2db6cd1a34e5cc0796c600f0f8ecfe9a3
Pavel Lamonov <leamonpaul@yandex.ru>
2020-06-24 23:40:20 +0300

    Update tests

--- Test.hs
+++ Test.hs
@@ -1,27 +1,22 @@
 import En (translateEn, parse)
 import System.IO
 
-translateArray :: [String] -> IO()
-translateArray [] = do
+translate :: [String] -> [String] -> [Int] -> IO()
+translate list dict [] = do
     return ()
-translateArray (x:xs) = do
-    handle <- openFile "en.dic" ReadMode
-    contents <- hGetContents handle
-    let translationLine = translateEn x (lines contents)
-    if translationLine /= ""
-        then do
-            putStrLn x
-            let translation = drop (length x + 1) translationLine
-            parse translation
-        else
-            putStrLn "Translation is not found."
-    hClose handle
-    translateArray xs
+translate list dict (x:xs) = do
+    putStrLn ("#" ++ (list !! x))
+    parse $ drop (length (list !! x) + 1) (dict !! x)
+    translate list dict xs
 
 main :: IO()
 main = do
-    handle <- openFile "list.dic" ReadMode
-    contents <- hGetContents handle
-    let array = (lines contents)
-    translateArray array
-    hClose handle
+    handleList <- openFile "list.dic" ReadMode
+    contentList <- hGetContents handleList
+    let list = (lines contentList)
+    handleDict <- openFile "en.dic" ReadMode
+    contentDict <- hGetContents handleDict
+    let dict = (lines contentDict)
+    translate list dict [0..(length list - 1)]
+    hClose handleList
+    hClose handleDict
